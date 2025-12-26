1c66b8fe23c8160d1cbadad37374ae575b1798a2
muhammedfariis <7591faris@gmail.com>
2025-12-26 16:18:25 +0530

    day.2

---
+++ javascript-practices/Day.2/day-2-dsa-practical.js
@@ -0,0 +1,31 @@
+let num = 15
+
+for(let a = 0 ; a<=num ; a++){
+    if(a%2===0){
+        console.log(a);
+        
+    }
+}
+
+for(let  i = 1 ; i<=num ; i++){
+    if(i%2===1){
+        console.log(i);
+        
+    }
+}
+
+
+let i = 1
+do{
+    console.log(i);
+    i++
+}while(i<=100)
+
+
+let k = 1 
+
+while(k<=100){
+    console.log(k);
+    k++
+}
+
---
+++ javascript-practices/Day.2/day2-methord.js
@@ -0,0 +1,30 @@
+// day 2 
+
+// parseInt , parseFloat , toFixed 
+// converts numbers to string , strings to numbers etc 
+
+
+const num = parseFloat(parseInt(10.5))
+
+console.log(num);
+
+let total = "10"
+let convertNum = Number(total) 
+
+console.log(convertNum);
+
+let decimals = 12.567
+let round = parseFloat(parseInt(decimals))
+let converts = Number(decimals.toFixed(2))
+
+console.log(round);
+console.log(converts);
+
+let numb = 12.65255255212145244
+let convert = Number(numb.toFixed(2))
+
+console.log(convert);
+
+
+
+
