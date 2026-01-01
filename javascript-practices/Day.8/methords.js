21233935e6a094a398337b3e20f97c8c1735ffc5
muhammedfariis <7591faris@gmail.com>
2026-01-01 16:01:32 +0530

    day.8

---
+++ javascript-practices/Day.8/methords.js
@@ -0,0 +1,13 @@
+// day 8 
+// array simple methord
+// push and pop shift unshift
+
+let array = [3,4]
+
+array.push(5,6)
+array.pop(6)
+array.unshift(0,1,2)
+array.shift(0)
+
+console.log(array);
+
