397ed2838154c7f2da6258d1efca6d6b776f993d
muhammedfariis <7591faris@gmail.com>
2026-01-02 15:11:12 +0530

    day.9

---
+++ javascript-practices/Day.9/methords.js
@@ -0,0 +1,18 @@
+// day 9 
+// slice and splice 
+
+let array = [10,20,30,40,50,60]
+
+let copying = array.slice(0,4)
+console.log(copying);
+
+let removing = array.splice(0,1)
+console.log(removing);
+
+// splicing means we have array of elements we want to delete some elements from array use splice 
+
+// the slice means we want get values from array use slice 
+
+// slice vs splice splice will delete and slice will get values from array 
+
+// this slice and splice have default for loops inside that function 
